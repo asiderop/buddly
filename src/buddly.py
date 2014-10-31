4572cd1b8b26913580f28cd672f297d324fd8c72
Alexander Sideropoulos <alexander@thequery.net>
2014-10-31 07:22:03 -0700

    db enhancements per docs

--- src/buddly.py
+++ src/buddly.py
@@ -21,21 +21,30 @@ def connect_db():
 
 def init_db():
     from contextlib import closing
-    with closing(connect_db()) as db:
+    with app.app_context():
+        db = get_db()
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
-@app.before_request
-def db_open():
-    g.db = connect_db()
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+        db.row_factory = sqlite3.Row
+    return db
 
-@app.teardown_request
-def db_close(exception):
-    db = getattr(g, 'db', None)
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
     if db is not None:
         db.close()
-
 
 #####################
 ## Routing
